863be5f72be47e97bc10a146fce5fa56a00c016e
Isaac Ngeno <isaac2ngeno5@gmail.com>
2018-10-14 17:42:53 +0300

    almost done with sign up UI datepicker remains

--- app/src/main/java/com/example/darkoscript/japp/SignUpActivity.java
+++ app/src/main/java/com/example/darkoscript/japp/SignUpActivity.java
@@ -2,12 +2,20 @@ package com.example.darkoscript.japp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class SignUpActivity extends AppCompatActivity {
-
+Spinner spinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        spinner = findViewById(R.id.spinner);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.counties_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
 }
